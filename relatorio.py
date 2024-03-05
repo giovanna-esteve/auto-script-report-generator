2625db9fd9be3b9146a67c60bfae7a03fa37f963
giovanna <giovanna.esteve@hotmail.com>
2024-03-05 13:41:24 -0300

    gerando apenas o relatorio final

--- relatorio.py
+++ relatorio.py
@@ -4,8 +4,7 @@ import web_scraping_goodreads as wsg
 
 df_world = wsg.mais_lidos_da_semana_no_mundo()
 df_brazil = wsg.mais_lidos_da_semana_no_brasil()
-
-file_name = wsg.pivot_table_mundo_brasil(df_world, df_brazil)
+file_name = wsg.pivot_table_brasil_mundo(df_world, df_brazil)
 
 wb = load_workbook(file_name)
 sheet = wb['graficos']
@@ -21,9 +20,9 @@ categories = Reference(sheet,
 barchart.add_data(data, titles_from_data=True)
 barchart.set_categories(categories)
 
-sheet.add_chart(barchart, "B10")
+sheet.add_chart(barchart, "B30")
 
-barchart.title = "Chart Title"
+barchart.title = "Leitores Brasileiros"
 barchart.style = 18
 
 sheet2 = wb['pivot_table']
@@ -39,9 +38,9 @@ categories2 = Reference(sheet2,
 barchart2.add_data(data2, titles_from_data=True)
 barchart2.set_categories(categories2)
 
-sheet.add_chart(barchart2, "B30")
+sheet.add_chart(barchart2, "B10")
 
-barchart2.title = "Chart two"
+barchart2.title = "Livros mais lidos no Mundo no goodreads"
 barchart2.style = 18
 
 wb.save(file_name)
--- web_scraping_goodreads.py
+++ web_scraping_goodreads.py
@@ -3,66 +3,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 from datetime import datetime
-import sys
-import os
-
-def mais_lidos_da_semana_no_brasil():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-
-    website = "https://www.goodreads.com/book/most_read?utf8=%E2%9C%93&country=BR&duration=w"
-    driver.get(website)
-
-    livros = driver.find_elements(by="xpath", value='//tr[@itemtype="http://schema.org/Book"]')
-
-    numeros = []
-    titulos = []
-    autores = []
-    leitores = []
-
-    for livro in livros:
-        numero = livro.find_element(by="xpath", value='./td').text
-        titulo = livro.find_elements(by="xpath", value='./td[3]//span[@itemprop="name"]')[0].text
-        autor = livro.find_elements(by="xpath", value='./td[3]//span[@itemprop="name"]')[1].text
-        quantidade = livro.find_element(by="xpath", value='./td[3]/span[@class="greyText statistic"]').text
-        qnt = quantidade.split(' ')
-        numeros.append(int(numero))
-        titulos.append(titulo)
-        autores.append(autor)
-        leitores.append(int(qnt[0]))
-
-    my_dict = {'numero':numeros, 'titulo':titulos, 'autor(a)':autores, 'leitores':leitores}
-    df = pd.DataFrame(my_dict)
-
-    now = datetime.now()
-    data = now.strftime("%d%m%Y")
-
-    # gerar excel no executavel relatorio.exe
-    '''
-    caminho = os.path.dirname(sys.executable)
-    df.to_excel(os.path.join(caminho, f'{data}_brazil_week.xlsx'))
-    '''
-
-    # gerar excel no arquivo relatorio.py
-    df.to_excel(f'{data}_brazil_week.xlsx')
-
-    driver.quit()
-    return df
-
 
 def mais_lidos_da_semana_no_mundo():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=service)
 
     website = "https://www.goodreads.com/book/most_read?category=all&country=all&duration=w"
     driver.get(website)
 
+    ##
     livros = driver.find_elements(by="xpath", value='//tr[@itemtype="http://schema.org/Book"]')
 
     numeros = []
@@ -87,32 +36,60 @@ def mais_lidos_da_semana_no_mundo():
     autores = autores[:20]
     leitores = leitores[:20]
 
-    my_dict = {'numero': numeros, 'titulo': titulos, 'autor(a)': autores, 'leitores': leitores}
+    my_dict = {'Numero': numeros, 'Titulo': titulos, 'Autor(a)': autores, 'Leitores': leitores}
+    ##
+
     df = pd.DataFrame(my_dict)
-
-    now = datetime.now()
-    data = now.strftime("%d%m%Y")
-
-    # gerar excel no executavel relatorio.exe
-    '''
-    caminho = os.path.dirname(sys.executable)
-    df.to_excel(os.path.join(caminho, f'{data}_world_week.xlsx'))
-    '''
-
-    # gerar excel no arquivo relatorio.py
-    df.to_excel(f'{data}_world_week.xlsx')
+    #now = datetime.now()
+    #data = now.strftime("%d%m%Y")
+    #df.to_excel(f'{data}_world_week.xlsx')
 
     driver.quit()
     return df
 
+def mais_lidos_da_semana_no_brasil():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
 
-def pivot_table_mundo_brasil(df_world, df_brazil):
+    website = "https://www.goodreads.com/book/most_read?utf8=%E2%9C%93&country=BR&duration=w"
+    driver.get(website)
 
-    df_world = df_world[['titulo', 'leitores', 'numero']]
+    ##
+    livros = driver.find_elements(by="xpath", value='//tr[@itemtype="http://schema.org/Book"]')
+
+    numeros = []
+    titulos = []
+    autores = []
+    leitores = []
+
+    for livro in livros:
+        numero = livro.find_element(by="xpath", value='./td').text
+        titulo = livro.find_elements(by="xpath", value='./td[3]//span[@itemprop="name"]')[0].text
+        autor = livro.find_elements(by="xpath", value='./td[3]//span[@itemprop="name"]')[1].text
+        quantidade = livro.find_element(by="xpath", value='./td[3]/span[@class="greyText statistic"]').text
+        qnt = quantidade.split(' ')
+        numeros.append(int(numero))
+        titulos.append(titulo)
+        autores.append(autor)
+        leitores.append(int(qnt[0]))
+
+    my_dict = {'Numero':numeros, 'Titulo':titulos, 'Autor(a)':autores, 'Leitores':leitores}
+    ##
+
+    df = pd.DataFrame(my_dict)
+    #now = datetime.now()
+    #data = now.strftime("%d%m%Y")
+    #df.to_excel(f'{data}_brazil_week.xlsx')
+
+    driver.quit()
+    return df
+
+def pivot_table_brasil_mundo(df_world, df_brazil):
+    df_world = df_world[['Titulo', 'Leitores', 'Numero']]
     list_world = df_world.values.tolist()
 
-    df_brazil = df_brazil[['titulo', 'leitores', 'numero']]
-    titles_brazil = df_brazil['titulo'].values.tolist()
+    df_brazil = df_brazil[['Titulo', 'Leitores', 'Numero']]
+    titles_brazil = df_brazil['Titulo'].values.tolist()
     list_brazil = df_brazil.values.tolist()
 
     local = []
@@ -135,11 +112,9 @@ def pivot_table_mundo_brasil(df_world, df_brazil):
             leitores.append(list_brazil[index][1])
             numeros.append(list_brazil[index][2])
 
-    my_dict = {'local': local, 'livro': livros, 'leitores': leitores, 'numeros':numeros}
+    my_dict = {'Local': local, 'Livro': livros, 'Leitores': leitores, 'Numeros': numeros}
     df = pd.DataFrame(my_dict)
-    #df.to_excel('aux_pivot.xlsx')
-    pivot_table_completo = df.pivot_table(index='local', columns='livro', values='leitores', aggfunc='sum')
-
+    pivot_table_completo = df.pivot_table(index='Local', columns='Livro', values='Leitores', aggfunc='sum')
 
     local = []
     livros = []
@@ -149,27 +124,18 @@ def pivot_table_mundo_brasil(df_world, df_brazil):
         if a in titles_brazil:
             titulo = a.split('(')[0]
 
-            local.append("mundo")
+            local.append("Mundo")
             livros.append(titulo)
             leitores.append(b)
             numeros.append(c)
 
-    my_dict = {'local': local, 'livro': livros, 'leitores': leitores, 'numeros':numeros}
+    my_dict = {'Local': local, 'Livro': livros, 'Leitores': leitores, 'Numeros': numeros}
     df = pd.DataFrame(my_dict)
-    pivot_table_mundo = df.pivot_table(index='local', columns='livro', values='leitores', aggfunc='sum')
+    pivot_table_mundo = df.pivot_table(index='Local', columns='Livro', values='Leitores', aggfunc='sum')
 
     now = datetime.now()
     data = now.strftime("%d%m%Y")
-
-    # gerar excel no executavel relatorio.exe
-    '''
-    caminho = os.path.dirname(sys.executable)
-    file_name = os.path.join(caminho, f'{data}_relatorio.xlsx')
-    '''
-
-    # gerar excel no arquivo relatorio.py
     file_name = f'{data}_relatorio.xlsx'
-
     with pd.ExcelWriter(file_name) as writer:
         pivot_table_completo.to_excel(writer, sheet_name="graficos", startrow=2)
         pivot_table_mundo.to_excel(writer, sheet_name="pivot_table", startrow=2)
